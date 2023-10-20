e6d2b880e57567aae751b28d064ff6426943b889
gintoki <satyamrocker1234@gmail.com>
2023-10-20 13:56:17 +0530

    added product database model and sample products for seeding

---
+++ src/models/productModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productModel = mongoose.model("productModel", productSchema);
+
+module.exports = productModel;

e4588313f3d404bf6d58c60f6836949fda1009a5
gintoki <satyamrocker1234@gmail.com>
2023-10-20 14:50:20 +0530

    Squashed commit of the following:
    commit 69c2b40f5ed10ebe074276fbe46c339a69540a1e
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Fri Oct 20 14:21:38 2023 +0530
    
        Added getProductByID route
    
    commit 7838bf7f1c70c5568db765ecc4fda13e15030cee
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Fri Oct 20 13:56:52 2023 +0530
    
        added getProduct route
    
    commit e6d2b880e57567aae751b28d064ff6426943b889
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Fri Oct 20 13:56:17 2023 +0530
    
        added product database model and sample products for seeding
    
    commit 47aa2be5436ef9709a6f99b576a6c023bce1d635
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Fri Oct 20 13:51:57 2023 +0530
    
        added seeder script
    
    commit 6432b177820c23d900572f7a2325c5a517ff99a9
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Thu Oct 19 15:21:53 2023 +0530
    
        added user details update route
    
    commit e2fb03f5fb469f2c09b87d0188e41b91b4c3615b
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Thu Oct 19 14:58:54 2023 +0530
    
        added user profile route
    
    commit ab01652168dafa9279ff1365a2f4f2552011ecff
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Thu Oct 19 14:21:19 2023 +0530
    
        added authentication middleware for request protection
    
    commit c9a8ce9dbc6e6bc86a757f9a268daa9bd7e67579
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Thu Oct 19 13:25:33 2023 +0530
    
        added user login system using passportjs
    
    commit 3aa8ec2af88e0afeaaefffa2f303a2512956de2e
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Wed Oct 18 18:57:51 2023 +0530
    
        added feature to send email on registration and email confirmation
    
    commit 1ed0f72baf874f8dd0f81ed9473bb7d033ede176
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Wed Oct 18 13:09:24 2023 +0530
    
        added registerUser route
    
    commit 7b18eb7ba1ee02640d902cb1f47ed0bdc2062fe2
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Wed Oct 18 12:32:04 2023 +0530
    
        created userModel
    
    commit 80253f6cd6ad9de6fa277bb650fb86e7ee5a33e8
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Wed Oct 18 12:25:23 2023 +0530
    
        added db connection
    
    commit 587950f45c45061c11f275123de1b060da7c4dfe
    Author: gintoki <satyamrocker1234@gmail.com>
    Date:   Tue Oct 17 14:10:36 2023 +0530
    
        created temporary routes for user routes

---
+++ src/controller/productController.js
@@ -0,0 +1,66 @@
+const Joi = require("joi");
+const productModel = require("../models/productModel");
+
+/**
+ * @desc    Fetch all products
+ * @route   GET /api/products
+ * @access  Public
+ * @param {query {page:int, limit:int}}: req
+ * this route requires pagination requires page number to passed in query
+ */
+const getProducts = async (req, res) => {
+  //schema for req object used for pagination of the request
+  const paginationSchema = Joi.object({
+    page: Joi.number().integer().min(1).optional(),
+    limit: Joi.number().integer().min(1).optional(),
+  });
+
+  try {
+    const { error } = paginationSchema.validate(req.query);
+
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    // Pagination parameters (page and limit)
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    // Calculate the skip value to skip the correct number of documents
+    const skip = (page - 1) * limit;
+
+    // Query the database to retrieve products with pagination
+    const products = await productModel.find().skip(skip).limit(limit);
+
+    // Count total products (for pagination info)
+    const totalProducts = await productModel.countDocuments();
+
+    res.status(200).json({
+      products,
+      totalPages: Math.ceil(totalProducts / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+/**
+ * @desc    Fetch product by given id
+ * @route   GET /api/products/:id
+ * @access  Public
+ * @param {{params:id}}: req
+ * this route requires pagination requires page number to passed in query
+ */
+const getProductById = async (req, res) => {
+  const product = await productModel.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ msg: "Product not found" });
+  }
+};
+
+module.exports = { getProducts, getProductById };
